b2b51b53e2476eb276a0d8d8a066b4e2ee565c5b
yorkblansh <yorkblansh@yandex.com>
2023-08-15 22:26:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-gjfvkq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-gjfvkq)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+import f from 'widehook/widehook';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
